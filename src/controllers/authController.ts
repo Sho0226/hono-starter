bc8fb6ea42d9331756792524358a87ed8de85cbb
Sho0226 <s1F102201616@iniad.org>
2024-09-29 19:06:31 +0900

    add: authContolloer

---
+++ src/controllers/authController.ts
@@ -0,0 +1,19 @@
+export const register = async (req: Request, res: Response) => {
+    const {name, email, password} = req.body;
+    try {
+        const user = await registerUser(name, email, password);
+        res.status(201).json(user);
+    }catch (error){
+        res.status(400).json({message: 'Registration failed'})
+    }
+};
+
+export const login = async (req: Request, res: Response) => {
+    const {email, password} = req.body;
+    try{
+        const token = await loginUser(email,password);
+        res.status(200).json({token})
+    }catch(error){
+        res.status(401).json({message:'Login failed'})
+    }
+};
